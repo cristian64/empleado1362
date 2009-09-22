eee2158300efdccfb9fc14c0dcc4d8b6ce83a7d0
cristian64 <cristian64@0785bf0e-8f45-11de-b034-ff552ed6835c>
2009-09-22 18:13:14 +0000

    Añadido el proyecto de pruebas de Bullet, con OpenGL.

---
+++ Pruebas OpenGL-Bullet/Bullet/BulletMultiThreaded/CMakeFiles/BulletMultiThreaded.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/BulletMultiThreaded.dir/SpuFakeDma.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuLibspe2Support.o"
+  "CMakeFiles/BulletMultiThreaded.dir/btThreadSupportInterface.o"
+  "CMakeFiles/BulletMultiThreaded.dir/Win32ThreadSupport.o"
+  "CMakeFiles/BulletMultiThreaded.dir/PosixThreadSupport.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SequentialThreadSupport.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuSampleTaskProcess.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuCollisionObjectWrapper.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuCollisionTaskProcess.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuGatheringCollisionDispatcher.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuContactManifoldCollisionAlgorithm.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuNarrowPhaseCollisionTask/boxBoxDistance.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuNarrowPhaseCollisionTask/SpuContactResult.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuNarrowPhaseCollisionTask/SpuMinkowskiPenetrationDepthSolver.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuNarrowPhaseCollisionTask/SpuEpaPenetrationDepthSolver.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuNarrowPhaseCollisionTask/SpuGjkEpa2.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuNarrowPhaseCollisionTask/SpuGatheringCollisionTask.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuNarrowPhaseCollisionTask/SpuVoronoiSimplexSolver.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuNarrowPhaseCollisionTask/SpuGjkPairDetector.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuNarrowPhaseCollisionTask/SpuCollisionShapes.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuParallelSolver.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuSolverTask/SpuParallellSolverTask.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuBatchRaycaster.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuRaycastTaskProcess.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuRaycastTask/SpuRaycastTask.o"
+  "CMakeFiles/BulletMultiThreaded.dir/SpuRaycastTask/SpuSubSimplexConvexCast.o"
+  "libBulletMultiThreaded.pdb"
+  "libBulletMultiThreaded.a"
+  "libBulletMultiThreaded.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/BulletMultiThreaded.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ Pruebas OpenGL-Bullet/Bullet/BulletMultiThreaded/CMakeFiles/BulletMultiThreaded.dir/cmake_clean_target.cmake
@@ -0,0 +1,4 @@
+FILE(REMOVE_RECURSE
+  "libBulletMultiThreaded.a"
+  "libBulletMultiThreaded.so"
+)
